ea6d5eba9c1ec2fa947aa84dc39f298eb42f1ae7
Edilianny Sanchez <edy992.6@gmail.com>
2018-01-03 16:01:34 +0000

    [REF] openacademy: Add to pyhon string method '_'

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import api, fields, models
+from openerp import api, fields, models, _
 
 
 class Course(models.Model):
@@ -19,11 +19,11 @@ class Course(models.Model):
         default = dict(default or {})
 
         copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
+            [('name', '=like', _(u"Copy of {}%").format(self.name))])
         if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
+            new_name = _(u"Copy of {}").format(self.name)
         else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
         default['name'] = new_name
         return super(Course, self).copy(default)
@@ -33,9 +33,9 @@ class Course(models.Model):
     _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
-         "The title of the course should not be the description"),
+         _("The title of the course should not be the description")),
 
         ('name_unique',
          'UNIQUE(name)',
-         "The course title must be unique"),
+         _("The course title must be unique")),
     ]
